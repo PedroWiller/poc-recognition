f6077f00d3b9b545c489f702d464c9a0eca2162e
Pedro Figueredo <pedrowiller25@gmail.com>
2025-10-04 20:56:27 -0300

    started new config project

---
+++ internal/config/aws/aws.go
@@ -0,0 +1,33 @@
+package aws
+
+import (
+	"context"
+	"log"
+
+	"poc-recognition/internal/config/env"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/rekognition"
+)
+
+type RekognitionService struct {
+	client *rekognition.Client
+	ctx    context.Context
+}
+
+func LoadConfig() aws.Config {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(env.AWS_REGION))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	return cfg
+}
+
+func NewSession() (*RekognitionService, error) {
+	cfg := LoadConfig()
+	return &RekognitionService{
+		client: rekognition.NewFromConfig(cfg),
+		ctx:    context.Background(),
+	}, nil
+}
---
+++ pkg/config/env/env.go
@@ -0,0 +1,5 @@
+package env
+
+func Load() {
+	// This is a placeholder for the Load function.
+}
